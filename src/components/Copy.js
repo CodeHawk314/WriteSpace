d0d398c3bf67497b46a364dbfff628bf54b9b500
Elijah Kim <reachelijah@gmail.com>
2021-02-04 15:20:38 -0700

    Broader copy to clipboard error handling

--- src/components/Copy.js
+++ src/components/Copy.js
@@ -41,22 +41,30 @@ function Copy({ writing, settings }) {
               msg: "Copied output picture to the clipboard!",
             });
           } catch (e) {
-            if (e.name === "ReferenceError") {
-              setSnackbar({
-                open: true,
-                success: false,
-                msg:
-                  "Sorry, copying images to the clipboard is not supported in your browser.",
-              });
-              return;
-            } else {
-              throw e;
-            }
+            setSnackbar({
+              open: true,
+              success: false,
+              msg:
+                "Sorry, copying images to the clipboard is not supported in your browser.",
+            });
+            console.error(e);
+            return;
           }
         });
         break;
       default:
-        await navigator.clipboard.writeText(toCopy);
+        try {
+          await navigator.clipboard.writeText(toCopy);
+        } catch (e) {
+          setSnackbar({
+            open: true,
+            success: false,
+            msg:
+              "Sorry, copying to the clipboard is not supported in your browser.",
+          });
+          console.error(e);
+          return;
+        }
         let msg;
         switch (format) {
           case "txt":
